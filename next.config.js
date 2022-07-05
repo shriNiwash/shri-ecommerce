b27e9ff0828f97c221146643e23e96de5eb1022e
shriNiwash <shriniwashsharma98413@gmail.com>
2022-07-05 18:49:49 +0545

    previous logs

--- next.config.js
+++ next.config.js
@@ -3,5 +3,4 @@ module.exports={
     images:{
         domains:["books.google.com"]
     },
-    pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
 }
--- pages/Payment.js
+++ pages/Payment.js
@@ -1,8 +1,6 @@
-import Khalti from "./components/khalti/khalti"
 const Payment = ()=>{
     return(<>
         <h1>Pay via following payment methods</h1>
-        <Khalti />
     </>)
 }
 

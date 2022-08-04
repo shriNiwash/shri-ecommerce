036455e48528474dfb1b9517bdbeb898da826c6a
shriNiwash <shriniwashsharma98413@gmail.com>
2022-08-04 16:07:27 +0545

    the nprogress is added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import Router from 'next/router'
+import Head from 'next/head';
+import NProgress from 'nprogress';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  Router.events.on("routeChangeStart",(url)=>{
+    console.log("Router is changing");
+    NProgress.start();
+
+  })
+  Router.events.on("routeChangeComplete",(url)=>{
+    console.log("Router is completed");
+    NProgress.done();
+  })
+  return (<>
+  <Head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"  />
+  </Head>
+  <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
